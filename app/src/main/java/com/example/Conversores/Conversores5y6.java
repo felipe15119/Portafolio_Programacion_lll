b7c4a5c2f55c459c26a56cc1ce3a2febcc5c9d93
AnibalLP <anibalinoz@gmail.com>
2020-08-23 00:37:02 -0600

    se agregaron las funcionalidades de los botones del activity inicial y tambien se comenzo el diseño del convertidor almacenamiento y volumen.

--- app/src/main/java/com/example/Conversores/Conversores5y6.java
+++ app/src/main/java/com/example/Conversores/Conversores5y6.java
@@ -1,14 +1,26 @@
 package com.example.Conversores;
 
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Conversores5y6 extends AppCompatActivity {
-
+TabHost tbhConvertir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversores5y6);
+        tbhConvertir=(TabHost)findViewById(R.id.thbconversor5y6);
+        tbhConvertir.setup();
+        tbhConvertir.addTab(tbhConvertir.newTabSpec("A").setContent(R.id.tbAlmacenamiento).setIndicator("ALMACENAMIENTO"));
+        tbhConvertir.addTab(tbhConvertir.newTabSpec("V").setContent(R.id.tbhVolumen).setIndicator("VOLUMEN"));
     }
+
 }
