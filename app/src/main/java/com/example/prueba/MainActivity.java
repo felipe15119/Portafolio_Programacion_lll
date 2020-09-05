be6914fd2aa094e083476ef14b42abedd92549c3
Felipe Martinez <Felipe151199/Programacion>
2020-09-04 21:16:58 -0600

    Creacion de Varias Intent

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.prueba;
 
 import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,23 +15,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText eNombre =
-                (EditText)findViewById(R.id.etNombre);
-        final EditText eEdad = (EditText)findViewById(R.id.etEdad);
-        Button bEnviar = (Button)findViewById(R.id.btEnviarDatos);
+
+        final EditText rNombre = (EditText)findViewById(R.id.etNombre);
+        Button bEnviar = (Button)findViewById(R.id.btEnviar);
+
         bEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nombre = eNombre.getText().toString();
-                String edad = eEdad.getText().toString();
-                Bundle pasarDatos = new Bundle();
-                pasarDatos.putString("pNombre",nombre);
-                pasarDatos.putString("pEdad",edad);
-                Intent siga = new
-                        Intent(MainActivity.this,Determinar.class);
-                siga.putExtras(pasarDatos);
-                startActivity(siga);
+                String dNombre = rNombre.getText().toString();
+
+                Bundle parametro = new Bundle();
+                parametro.putString("dNombre", dNombre);
+
+                Intent paso = new Intent(MainActivity.this,RecibirDatos.class);
+                paso.putExtras(parametro);
+                startActivity(paso);
             }
         });
+
+
     }
 }
