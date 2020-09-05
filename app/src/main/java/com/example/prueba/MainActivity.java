569bae4696adf215ad7eca6d4dcdccec9c22dd95
Felipe Martinez <Felipe151199/Programacion>
2020-09-04 21:01:15 -0600

    Creacion de Dos activitys para enviar datos de una a otra

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.prueba;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -14,7 +13,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText eNombre =
+                (EditText)findViewById(R.id.etNombre);
+        final EditText eEdad = (EditText)findViewById(R.id.etEdad);
+        Button bEnviar = (Button)findViewById(R.id.btEnviarDatos);
+        bEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = eNombre.getText().toString();
+                String edad = eEdad.getText().toString();
+                Bundle pasarDatos = new Bundle();
+                pasarDatos.putString("pNombre",nombre);
+                pasarDatos.putString("pEdad",edad);
+                Intent siga = new
+                        Intent(MainActivity.this,Determinar.class);
+                siga.putExtras(pasarDatos);
+                startActivity(siga);
+            }
+        });
     }
-
-
 }
+
